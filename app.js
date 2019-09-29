b4d3cb88ce71a92ad054e1211df8890624aa42e9
odafe1 <godwin.otokina@ikooba.com>
2019-09-30 00:39:15 +0100

    finished the project

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const userRoutes = require('./routes/user');
+const stuffRoutes = require("./routes/stuff");
+const path = require('path');
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://wincodes:mp9e9qvGmy8h37Qk@cluster0-0ogoh.mongodb.net/mean_stack?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Successfully connected to MongoDB Atlas!");
+  })
+  .catch(error => {
+    console.log("Unable to connect to MongoDB Atlas!");
+    console.error(error);
+  });
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+
+app.use("/api/stuff", stuffRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
